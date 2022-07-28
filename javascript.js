0e55aadf958973a91c3cb93a5a6ce4151bcaf8ae
frantic-dev <107622593+frantic-dev@users.noreply.github.com>
2022-07-28 15:33:36 +0100

    color grey when hover

--- javascript.js
+++ javascript.js
@@ -1,9 +1,16 @@
 const container = document.querySelector("#container");
 
-function createDivs() {
-    const div = document.createElement("div");
-     container.appendChild(div);
+function createDivs(i) {
+    let div = document.createElement("div");
+    div.classList.add(i);
+    div.setAttribute('onmouseover', "colorGrey(this)")
+    container.appendChild(div);
 }
+
+function colorGrey(div) {
+    div.style.backgroundColor = 'rgba(116, 116, 116, 0.878)';
+}
+
 for (let i = 1 ; i <= 256; i++) {
-    createDivs()
+    createDivs(i)
 }
